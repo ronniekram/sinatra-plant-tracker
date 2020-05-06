80a470955c4e33fbcdce7c4f00e9f2cae18600ef
github username <github email address>
2020-05-06 16:09:05 -0700

    added route skeletons

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,23 @@
 class PlantsController < ApplicationController 
+
+  get '/plants' do 
+  end 
+
+  get '/plants/new' do 
+  end 
+
+  post '/plants' do 
+  end 
+
+  get '/plants/:id' do 
+  end 
+
+  get '/plants/:id/edit' do 
+  end 
+
+  patch '/plants/:id' do 
+  end 
+
+  delete '/plants/:id' do 
+  end 
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController 
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/signup' do 
+  end 
+
+  post '/signup' do 
+  end 
+
+  get '/login' do 
+  end 
+
+  post '/login' do 
+  end 
+
+  get '/logout' do 
+  end 
 end 
